1efd760655ab086a98fb69caee849f45259cca44
Zura Aleksaia <zura@localhost.localdomain>
2024-04-24 13:40:18 +0400

    Added logging messages to a file

--- server/server.go
+++ server/server.go
@@ -14,6 +14,26 @@ func main() {
 	connectionMap := &sync.Map{}
 	server, err := net.Listen("tcp", ":5555")
 	fmt.Println("MOGESALMEBI PAPUNITO")
+	var logfile *os.File
+	filename := "logfile.txt"
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		fmt.Println("Log file does not exist, creating it")
+		file, err := os.Create(filename)
+		if err != nil {
+			fmt.Println("Error creating file: ", err.Error())
+		}
+		logfile = file
+		defer file.Close()
+	} else {
+		fmt.Println("Log file exists, opening it")
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Println("Error opening file: ", err.Error())
+		}
+		logfile = file
+		defer file.Close()
+	}
 
 	if err != nil {
 		fmt.Println("NAXUI BICHO: ", err.Error())
@@ -34,11 +54,11 @@ func main() {
 
 		fmt.Println("CONNECTIONS: ", connectionMap)
 
-		go handleConnection(connection, connectionCount-1, connectionMap)
+		go handleConnection(connection, connectionCount-1, connectionMap, logfile)
 	}
 }
 
-func handleConnection(connection net.Conn, id int, connectionMap *sync.Map) {
+func handleConnection(connection net.Conn, id int, connectionMap *sync.Map, logfile *os.File) {
 	defer func() {
 		connection.Close()
 		connectionMap.Delete(strconv.Itoa(id))
@@ -50,6 +70,11 @@ func handleConnection(connection net.Conn, id int, connectionMap *sync.Map) {
 		if err != nil {
 			fmt.Println("NAXUI BICHOrr: ", err.Error())
 			return
+		}
+
+		_, err = logfile.WriteString(buff)
+		if err != nil {
+			fmt.Println("Error writing to file: ", err.Error())
 		}
 
 		connectionMap.Range(func(key, value interface{}) bool {
